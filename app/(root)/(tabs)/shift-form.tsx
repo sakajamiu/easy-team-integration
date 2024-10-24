f43eebaa403b65ab20e2636623fd76613c003934
jamiu <jamiusaka.a@gmail.com>
2024-10-24 08:52:06 +0100

    added shift form

--- app/(root)/(tabs)/shift-form.tsx
+++ app/(root)/(tabs)/shift-form.tsx
@@ -1,10 +1,64 @@
-import { Text } from "react-native";
+import { router, useLocalSearchParams, useNavigation} from "expo-router";
+import { useRef, useLayoutEffect , useEffect} from "react";
+import { ShiftForm, ShiftFormRef } from "@easyteam/ui";
+import { Alert, Platform } from "react-native";
+import { HeaderBackButton} from '@react-navigation/elements'
 import { SafeAreaView } from "react-native-safe-area-context";
-const ShiftForm = () => {
-  return (
-    <SafeAreaView>
-      <Text>Shift Form</Text>
-    </SafeAreaView>
-  );
-};
-export default ShiftForm;
+import dayjs from "dayjs";
+
+ export default function ShiftFormScreen() {
+    const params = useLocalSearchParams()
+    const { employeeId, date  } = params;
+    const navigation = useNavigation()
+    const ref = useRef<ShiftFormRef>(null);
+    useLayoutEffect (() => {
+    // Modify the screen title and add a cancel button to the header
+    const screenTitle = date? dayjs(date as string).format('ddd,MMM DD'): "Add Shift";
+    const headerLeft = Platform.select({
+    // eslint-disable-next-line react/no-unstable-nested-components
+    ios: () => <HeaderBackButton tintColor="#ff3479" onPress={() => navigation.goBack()} />,
+    default: undefined,
+    });
+    navigation.setOptions({
+    title: screenTitle,
+    headerLeft,
+    });
+    
+}, [navigation, date, params, router]);
+useEffect (() => {
+    const preventGoingBack = (e: any) => {
+        if (!ref.current?. unsavedChanges) {
+        }
+        // If we don't have unsaved changes, then we don't need to do anything return;
+        // Prevent default behavior of leaving the screen
+        e.preventDefault();
+        Alert.alert("Unsaved Changes", "Are you sure you want to discard the changes?", [
+            { text: "Cancel", style: "cancel", onPress: () => {} },
+            {
+                     // Prompt the user before leaving the screen
+        
+        text: "Yes",
+        style: "destructive",
+        // If the user confirmed, then we dispatch the action we blocked earlier
+        // This will continue the action that had triggered the removal of the screen 
+        onPress: () => navigation.dispatch(e.data.action),
+            },
+            ]);
+        };
+       
+   
+        const unsubscribe= navigation.addListener("beforeRemove", preventGoingBack);
+        return unsubscribe;
+        
+}, [navigation, ref])
+
+return (
+    <SafeAreaView className="bg-[#333]">
+    <ShiftForm
+ref={ref}
+employeeId={employeeId as string} shiftDate={date as string}
+onSaveSuccess={() => navigation.goBack()} onCancelPress={() => navigation.goBack()} onEvent={event => console.log(event)}
+/>
+</SafeAreaView>);
+ }
+
